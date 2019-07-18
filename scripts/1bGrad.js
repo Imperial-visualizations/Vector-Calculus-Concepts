88002958579c079853687e348e49ca67ef3b98fb
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2019-07-18 16:02:20 +0100

    updates a with scalar graph

--- scripts/1bGrad.js
+++ scripts/1bGrad.js
@@ -63,7 +63,19 @@ function testPlot(){
                     l: 65,
                     r: 50,
                     b: 65,
-                    t: 90}
+                    t: 90},
+        dragmode: 'orbit',
+        scene: {
+            aspectmode: "cube",
+            xaxis: {range: [xMin, xMax], title: 'x'},
+            yaxis: {range: [yMin, yMax], title: 'y'},
+            zaxis: {range: [-10, 10], title: 'f(x,y)'},
+
+            camera: {
+                up: {x: 0, y: 0, z: 1},//sets which way is up
+                eye: {x: -1, y: -1, z: 1}//adjust camera starting view
+            }
+        },
     };
 
     console.log(xScalarPlot);
@@ -72,5 +84,72 @@ function testPlot(){
     Plotly.newPlot('Scalar_Graph_1b', dataPlot1b, layout, {showSendToCloud: true});
 };
 
+function testPlot2 (){
 
-$(document).ready(testPlot); //Load setup when document is ready.
+};
+
+function updatePlot(){
+    let a1b = parseFloat(document.getElementById('Slider_1').value);
+    let sigma1b = 10;
+    let xMin = -20;
+    let xMax = 20;
+    let yMin = -20;
+    let yMax = 20;
+    let plotStep = 1;
+    let ScalarPlot = [];
+
+    ScalarPlot = setupData(xMin, xMax, yMin, yMax, plotStep);
+
+    let xScalarPlot = ScalarPlot[0];
+    let yScalarPlot = ScalarPlot[1];
+
+    zScalarPlot = gaussian1b(a1b, sigma1b, xScalarPlot, yScalarPlot);
+
+    dataPlot1b = dataCompile(xScalarPlot, yScalarPlot, zScalarPlot);
+
+    let layout = {
+        title: 'Mt Bruno Elevation',
+        autosize: false,
+        width: 500,
+        height: 500,
+        margin: {
+                    l: 65,
+                    r: 50,
+                    b: 65,
+                    t: 90},
+        dragmode: 'orbit',
+        scene: {
+            aspectmode: "cube",
+            xaxis: {range: [xMin, xMax], title: 'x'},
+            yaxis: {range: [yMin, yMax], title: 'y'},
+            zaxis: {range: [-10, 10], title: 'f(x,y)'},
+
+            camera: {
+                up: {x: 0, y: 0, z: 1},//sets which way is up
+                eye: {x: -1, y: -1, z: 1}//adjust camera starting view
+            }
+        },
+    };
+
+    Plotly.animate(
+        'Scalar_Graph_1b', {
+        data: dataPlot1b, layout: layout,
+
+            fromcurrent: true,
+            transition: {duration: 0,},
+            frame: {duration: 0, redraw: false,},
+            mode: "immediate"
+        }
+    );
+};
+
+function main(){
+    testPlot()
+    $('#Slider_1').on("input", function(){
+        //update plots when coefficient changed
+        $("#" + $(this).attr("id") + "Display").text($(this).val() + $("#" + $(this).attr("id") + "Display").attr("data-unit"));
+        updatePlot();
+    });
+};
+
+$(document).ready(main); //Load setup when document is ready.

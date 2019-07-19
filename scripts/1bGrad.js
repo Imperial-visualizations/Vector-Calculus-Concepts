aab83a5be89e622e0913d458ea2bbcd22ec0bfa4
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2019-07-19 10:56:36 +0100

    Slightly optimised

--- scripts/1bGrad.js
+++ scripts/1bGrad.js
@@ -1,36 +1,3 @@
-let a1b = 5;
-let sigma1b = 10;
-let xMin = -20;
-let xMax = 20;
-let yMin = -20;
-let yMax = 20;
-let plotStep = 1;
-let ScalarPlot = [];
-
-const layout_1b = {
-            title: 'Mt Bruno Elevation',
-            autosize: false,
-            width: 500,
-            height: 500,
-            margin: {
-                        l: 65,
-                        r: 50,
-                        b: 65,
-                        t: 90},
-            dragmode: 'orbit',
-            scene: {
-                aspectmode: "cube",
-                xaxis: {range: [xMin, xMax], title: 'x'},
-                yaxis: {range: [yMin, yMax], title: 'y'},
-                zaxis: {range: [-10, 10], title: 'f(x,y)'},
-
-                camera: {
-                    up: {x: 0, y: 0, z: 1},//sets which way is up
-                    eye: {x: -1, y: -1, z: 1}//adjust camera starting view
-                }
-            },
-        };
-
 function setupData(xMin, xMax, yMin, yMax, plotStep){
     let xScalar1b = [];
     let yScalar1b = [];
@@ -63,57 +30,32 @@ function dataCompile(xScalar1b,yScalar1b,zScalar1b){
          let dataPlot = [{
                              x:xScalar1b,
                              y:yScalar1b,
-                             z: zScalar1b, type: 'surface'
+                             z: zScalar1b,
+                             type: 'surface',
+                             showscale: false
                          }];
     return dataPlot
 };
 
-function testPlot(){
-    let a1b = 5;
-    let sigma1b = 10;
-    let xMin = -20;
-    let xMax = 20;
-    let yMin = -20;
-    let yMax = 20;
-    let plotStep = 1;
-    let ScalarPlot = [];
-
-    ScalarPlot = setupData(xMin, xMax, yMin, yMax, plotStep);
-
-    let xScalarPlot = ScalarPlot[0];
-    let yScalarPlot = ScalarPlot[1];
-
-    zScalarPlot = gaussian1b(a1b, sigma1b, xScalarPlot, yScalarPlot);
+function testPlot(xMin, xMax, yMin, yMax, plotStep, xScalarPlot, yScalarPlot, a1b, sigma1b,layout_1b){
+    let zScalarPlot = gaussian1b(a1b, sigma1b, xScalarPlot, yScalarPlot);
 
     dataPlot1b = dataCompile(xScalarPlot, yScalarPlot, zScalarPlot);
-
-//    let layout = layout_1b;
-
-    console.log(xScalarPlot);
-    console.log(zScalarPlot);
-    console.log(dataPlot1b);
-    Plotly.newPlot('Scalar_Graph_1b', dataPlot1b, layout_1b, {showSendToCloud: true});
+    Plotly.react('Scalar_Graph_1b', dataPlot1b, layout_1b, {showSendToCloud: true});
 };
 
 function testPlot2 (){
 
 };
 
-function updatePlot(){
+function updatePlot(xMin, xMax, yMin, yMax, plotStep, xScalarPlot, yScalarPlot, sigma1b,layout_1b){
     let a1b = parseFloat(document.getElementById('Slider_1').value);
-
-
-    ScalarPlot = setupData(xMin, xMax, yMin, yMax, plotStep);
-
-    let xScalarPlot = ScalarPlot[0];
-    let yScalarPlot = ScalarPlot[1];
 
     zScalarPlot = gaussian1b(a1b, sigma1b, xScalarPlot, yScalarPlot);
 
     dataPlot1b = dataCompile(xScalarPlot, yScalarPlot, zScalarPlot);
 
-    let layout = layout_1b;
-
+//    let layout = layout_1b;
     Plotly.animate(
         'Scalar_Graph_1b', {
         data: dataPlot1b, layout: layout_1b,
@@ -127,11 +69,47 @@ function updatePlot(){
 };
 
 function main(){
-    testPlot()
+    let a1b = 5;
+    let sigma1b = 10;
+    let xMin = -20;
+    let xMax = 20;
+    let yMin = -20;
+    let yMax = 20;
+    let plotStep = 1;
+    const layout_1b = {
+            title: 'Mt Bruno Elevation',
+            autosize: false,
+            width: 500,
+            height: 500,
+            margin: {
+                        l: 65,
+                        r: 50,
+                        b: 65,
+                        t: 90},
+            dragmode: 'orbit',
+            scene: {
+                aspectmode: "cube",
+                xaxis: {range: [xMin, xMax], title: 'x'},
+                yaxis: {range: [yMin, yMax], title: 'y'},
+                zaxis: {range: [-10, 10], title: 'f(x,y)'},
+
+                camera: {
+                    up: {x: 0, y: 0, z: 1},//sets which way is up
+                    eye: {x: -1, y: -1, z: 1}//adjust camera starting view
+                }
+            },
+        };
+
+    let ScalarPlot = setupData(xMin, xMax, yMin, yMax, plotStep);
+
+    let xScalarPlot = ScalarPlot[0];
+    let yScalarPlot = ScalarPlot[1];
+
+    testPlot(xMin, xMax, yMin, yMax, plotStep, xScalarPlot, yScalarPlot, a1b, sigma1b,layout_1b);
     $('#Slider_1').on("input", function(){
         //update plots when coefficient changed
         $("#" + $(this).attr("id") + "Display").text($(this).val() + $("#" + $(this).attr("id") + "Display").attr("data-unit"));
-        updatePlot();
+        updatePlot(xMin, xMax, yMin, yMax, plotStep, xScalarPlot, yScalarPlot, sigma1b,layout_1b);
     });
 };
 

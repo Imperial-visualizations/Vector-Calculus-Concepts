53344a31ab7e47eb6cded2f512b2928b31c4ec02
Amolition <amolbhuptani@gmail.com>
2019-10-06 20:00:47 +0100

    super-final formatting done, files for upload in Upload Ready folder

--- iFrame-Version/scripts/VC-scripts/0Intro.js
+++ iFrame-Version/scripts/VC-scripts/0Intro.js
@@ -13,8 +13,8 @@ function displayImage(){
 
 function main(){
 //jQuery to update the plot as the value of the slider changes.
-    document.getElementById("scalarFieldImage").style.display = "block"
-    document.getElementById("vectorFieldImage").style.display = "none"
+    document.getElementById("scalarFieldImage").style.display = "block";
+    document.getElementById("vectorFieldImage").style.display = "none";
     $("input[type=radio]").each(function () {
         /*Allows for live update for display values*/
         $(this).on('input', function(){
